ace2c88b403ed2f6dbdd8f601be97c0144424830
Gustavo Naoto Aguilar Morita <macrofis@gmail.com>
2019-09-26 20:50:10 -0300

    Se agrega transacción de venta: update y select de todas las ventas.

--- FeriaVirtualServices/Structures/Tables/Ventas.cs
+++ FeriaVirtualServices/Structures/Tables/Ventas.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace FeriaVirtualServices.Structures.Tables
 {
-    class Ventas
+    public class Ventas
     {
+        public int id { get; set; }
+        public string username { get; set; }
+        public DateTime fecha { get; set; }
+        public string tipoEstado { get; set; }
+        public string tipoVenta { get; set; }
+
+        public Ventas(int id, string username, DateTime fecha, string tipoEstado, string tipoVenta)
+        {
+            this.id = id;
+            this.username = username;
+            this.fecha = fecha;
+            this.tipoEstado = tipoEstado;
+            this.tipoVenta = tipoVenta;
+        }
     }
 }
